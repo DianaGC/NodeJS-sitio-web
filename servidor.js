fb8f8a7a9d9c079fa53cca5ef2c4eef1a3b2ffc9
DianaGC <gdianaeliz@gmail.com>
2020-04-27 19:22:02 -0500

    Adding basic server

---
+++ servidor.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.write("hola Daina");
+    response.end();
+
+}).listen(3000, 'localhost');
+
+console.log('El servidor se muestra en http://localhost:3000')

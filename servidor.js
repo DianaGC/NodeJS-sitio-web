de0488e5b591e012f7dfdc225ad1ea958db7b1d4
DianaGC <gdianaeliz@gmail.com>
2020-04-29 16:14:21 -0500

    Adding basic routes in nodeJS

--- servidor.js
+++ servidor.js
@@ -1,9 +1,21 @@
+var fs = require('fs');
 var http = require('http');
 http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type':'text/plain'});
-    response.write("Martes");
-    response.end();
+    response.writeHead(200,{'Content-Type':'text/html'});
+    switch(request.url){
+        case '/':
+            page = "index.html"; 
+            break;
+        case '/nodejs':
+            page = "info.html";
+            break;
+        default:
+            page = "404.html";
+            break;
+    }
+    fs.readFile("./pages/"+page, function(error, datos){
+        response.write(datos);
+        response.end();
+    });
 
 }).listen(3000, 'localhost');
-
-console.log('El servidor se muestra en http://localhost:3000')

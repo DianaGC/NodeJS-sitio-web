c7356e8da22c21f373d6e7c4586ed7e826f0462e
DianaGC <gdianaeliz@gmail.com>
2020-05-02 19:09:51 -0500

    Adding array in pug

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ var fs = require('fs');
 var app = express();
 
 const pug = require('pug');
-//definiendo una ruta
+//definiendo una ruta por defecto  /public
 app.use(express.static(__dirname + '/public'))
+tshirtsList=[
+    {color:"orange", image:"images/3.jpeg"},
+    {color:"black", image:"images/4.jpeg"},
+    {color:"orange", image:"images/5.jpeg"},
+    {color:"black", image:"images/6.jpeg"},
+    {color:"black", image:"images/7.jpeg"}
+]
 
 app.get('/', function(request, response){
     response.render('index.pug', {
         title: "Test",
-        text: "xxx"
+        text: "We store of shits offer the beter"
     });
 });
+
+app.get('/store.html', function(request, response){
+    response.render('store.pug',{
+        tshirts:tshirtsList
+    })
+})
 app.listen(3000, function(){
     console.log('Listening express in the port 3000')
 })

840cd3c21b207ab426d9a2090772ddf0989e857a
DianaGC <gdianaeliz@gmail.com>
2020-05-03 20:36:37 -0500

    Adding routes

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const pug = require('pug');
 //definiendo una ruta por defecto  /public
 app.use(express.static(__dirname + '/public'))
 tshirtsList=[
-    {color:"orange", image:"images/3.jpeg"},
+    {color:"orange", image:"images/2.jpeg"},
+    {color:"grey", image:"images/3.jpeg"},
     {color:"black", image:"images/4.jpeg"},
-    {color:"orange", image:"images/5.jpeg"},
-    {color:"black", image:"images/6.jpeg"},
-    {color:"black", image:"images/7.jpeg"}
+    {color:"green", image:"images/5.jpeg"},
+    {color:"red", image:"images/6.jpeg"},
+    {color:"blue", image:"images/7.jpeg"}
 ]
 
 app.get('/', function(request, response){
@@ -25,6 +26,28 @@ app.get('/store.html', function(request, response){
     response.render('store.pug',{
         tshirts:tshirtsList
     })
+});
+
+app.get('/store/buy/:color', function(request, response){
+    let dataTshirt = tshirtsList.filter(function(item){
+        if(request.params.color == item.color){
+            console.log('iiiii',item)
+            return item
+        }
+    })[0]
+    response.render('details.pug',{
+        color:request.params.color,
+        datos: dataTshirt
+    })
+
+})
+
+app.use(function(request, response){
+    let URLError = request.originalUrl
+    response.status(400);
+    response.render('404.pug',{
+        textError: URLError
+    });
 })
 app.listen(3000, function(){
     console.log('Listening express in the port 3000')
